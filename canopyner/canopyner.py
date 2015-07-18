0a2842b8d7cdae2d0c6da339719948c5bc7cc0f5
Kyle Altendorf <sda@fstab.net>
2015-07-18 16:14:59 -0400

    Add object dictionary import dialog

--- canopyner/canopyner.py
+++ canopyner/canopyner.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-from PyQt5.QtWidgets import QMainWindow
+from collections import OrderedDict
+
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
 
 import eds
 import od
@@ -14,6 +16,35 @@ class CANopyner(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.od = None
+        self.model = None
+        if file is not None:
+            self.import_od(file)
+
+        self.ui.tree.clicked.connect(self.item_clicked)
+        self.ui.actionImport_Object_Dictionary.triggered.connect(self.import_od)
+
+    def item_clicked(self, index):
+        self.ui.label.setText(str(index.model().node_from_index(index)))
+
+    def import_od(self, checked=False, file=None):
+        print(file)
+        if file is None:
+            types = OrderedDict()
+            types['Electronic Data Sheet'] = ['eds']
+            # TODO  add XDD support
+            # types['XDD'] = ['xdd']
+
+            filters = []
+            for name, extensions in types.items():
+                ext = ', '.join(['*.' + e for e in extensions])
+                filters.append('{n} ({e}) ({e})'.format(n=name, e=ext))
+            print(';;'.join(filters))
+
+            file = QFileDialog.getOpenFileName(self, 'Open Object Dictionary',
+                                               filter=';;'.join(filters))[0]
+            file = file if len(file) > 0 else None
+
         if file is not None:
             self.od = eds.parse(file)
             # TODO   don't sort here...
@@ -22,15 +53,8 @@ class CANopyner(QMainWindow):
                 # TODO   don't sort here...
                 child.children.sort()
             self.model = od.ObjectDictionaryModel(self.od)
-        else:
-            self.model = TreeModel()
 
-        self.ui.tree.setModel(self.model)
-
-        self.ui.tree.clicked.connect(self.item_clicked)
-
-    def item_clicked(self, index):
-        self.ui.label.setText(str(index.model().node_from_index(index)))
+            self.ui.tree.setModel(self.model)
 
 
 if __name__ == '__main__':
@@ -40,7 +64,7 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
 
-    canopyner = CANopyner(file=sys.argv[1])
+    canopyner = CANopyner()
     canopyner.show()
 
     sys.exit(app.exec_())
