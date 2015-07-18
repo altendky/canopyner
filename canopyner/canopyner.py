916dfec94d31d52a43531abac2aca8350e17c69e
Kyle Altendorf <sda@fstab.net>
2015-07-18 14:32:55 -0400

    Switch to .ui from Qt Designer

--- canopyner/canopyner.py
+++ canopyner/canopyner.py
@@ -1,30 +1,18 @@
 #!/usr/bin/env python3
 
-from PyQt5.QtWidgets import (QHBoxLayout, QWidget, QTreeView, QMainWindow,
-                             QDockWidget, QLabel)
+from PyQt5.QtWidgets import QMainWindow
 
-from PyQt5.QtCore import Qt
 import eds
 import od
+from generated.canopyner_ui import Ui_MainWindow
 
 
 class CANopyner(QMainWindow):
     def __init__(self, file=None, parent=None):
         super(CANopyner, self).__init__(parent)
 
-        self.setDockOptions(
-            QMainWindow.AnimatedDocks | QMainWindow.AllowNestedDocks)
-
-        self.tree = QTreeView()
-        self.od_dock = QDockWidget("OD", self)
-        self.od_dock.setWidget(self.tree)
-        self.addDockWidget(Qt.LeftDockWidgetArea, self.od_dock)
-
-        self.label = QLabel()
-        self.label.setAlignment(Qt.AlignCenter)
-        self.data_dock = QDockWidget("Data", self)
-        self.data_dock.setWidget(self.label)
-        self.addDockWidget(Qt.RightDockWidgetArea, self.data_dock)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
         if file is not None:
             self.od = eds.parse(file)
@@ -37,13 +25,12 @@ class CANopyner(QMainWindow):
         else:
             self.model = TreeModel()
 
-        self.tree.setModel(self.model)
-        self.resize(500, 500)
+        self.ui.tree.setModel(self.model)
 
-        self.tree.clicked.connect(self.item_clicked)
+        self.ui.tree.clicked.connect(self.item_clicked)
 
     def item_clicked(self, index):
-        self.label.setText(str(index.model().node_from_index(index)))
+        self.ui.label.setText(str(index.model().node_from_index(index)))
 
 
 if __name__ == '__main__':
---
+++ generate.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+
+def find_files_by_type(path, extension):
+    if not extension.startswith('.'):
+        extension = '.' + extension
+
+    matches = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(extension):
+                matches.append(os.path.join(root, file))
+
+    return matches
+
+def generate():
+    for f in find_files_by_type('.', '.ui'):
+        print('Processing: ' + f)
+
+        file_split = os.path.splitext(os.path.basename(f))
+
+        if file_split[1] != '.ui':
+            print(os.path.basename(__file__) + ': Invalid extension for UI file: ' +
+                  file_split[1],
+                  file=sys.stderr)
+        else:
+            generated_directory = os.path.join(os.path.dirname(f), 'generated')
+            try:
+                os.mkdir(generated_directory)
+            except FileExistsError:
+                if os.path.isdir(generated_directory):
+                    pass
+                else:
+                    raise
+
+            output = os.path.join(generated_directory, file_split[0] + '_ui.py')
+
+            subprocess.call(['pyuic5', f, '-o', output])
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(generate())
