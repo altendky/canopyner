0ee5abd65613715ab59b1facf70bd3a34dc98a78
Kyle Altendorf <sda@fstab.net>
2015-07-19 17:01:43 -0400

    Generalize generation

--- generate.py
+++ generate.py
@@ -5,43 +5,60 @@ import subprocess
 import sys
 
 
-def find_files_by_type(path, extension):
+# can be helpful cleaning... find . -name 'generated' -type d -exec rm -rf {} \;
+
+def find_files_by_type(path, extension, exclude_dirs=[]):
     if not extension.startswith('.'):
         extension = '.' + extension
 
+    exclude_dirs = [os.path.abspath(d) for d in exclude_dirs]
+
     matches = []
     for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith(extension):
-                matches.append(os.path.join(root, file))
+        if True not in [os.path.abspath(root).startswith(d) for d in
+                        exclude_dirs]:
+            for file in files:
+                if file.endswith(extension):
+                    matches.append(os.path.join(root, file))
 
     return matches
 
 
-def generate():
-    for f in find_files_by_type('.', '.ui'):
+def make_dir_ready(path):
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        if os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def generic(extension, program, out_lambda, in_option=None, out_option=None,
+            out_dir_lambda=lambda d: os.path.join(d, 'generated'),
+            in_dir_exclude=[]):
+    if not extension.startswith('.'):
+        extension = '.' + extension
+
+    for f in find_files_by_type('.', extension, in_dir_exclude):
         print('Processing: ' + f)
 
-        file_split = os.path.splitext(os.path.basename(f))
+        out_dir = out_dir_lambda(os.path.dirname(f))
+        make_dir_ready(out_dir)
+        out_file = os.path.join(out_dir, out_lambda(os.path.basename(f)))
 
-        if file_split[1] != '.ui':
-            print(os.path.basename(__file__) +
-                  ': Invalid extension for UI file: ' +
-                  file_split[1],
-                  file=sys.stderr)
-        else:
-            generated_directory = os.path.join(os.path.dirname(f), 'generated')
-            try:
-                os.mkdir(generated_directory)
-            except FileExistsError:
-                if os.path.isdir(generated_directory):
-                    pass
-                else:
-                    raise
+        command_line = [program, in_option, f, out_option, out_file]
+        command_line = [e for e in command_line if e is not None]
 
-            output = os.path.join(generated_directory, file_split[0] + '_ui.py')
+        print('Executing: ' + ' '.join(command_line))
+        subprocess.call(command_line)
 
-            subprocess.call(['pyuic5', f, '-o', output])
+
+def generate():
+    generic(extension='.ui',
+            program='pyuic5',
+            out_lambda=lambda f: os.path.splitext(f)[0] + '_ui.py',
+            out_option='-o')
 
     return 0
 
